5465701127f49547b1e5f9a6074bf07755785473
exploitforge <tanyibesong.bryanmaurice@ictuniversity.edu.cm>
2026-01-25 05:01:54 +0100

    Add files via upload

---
+++ myscript.sh
@@ -0,0 +1,10 @@
+#!/bin/bash 
+name=Besong
+myage=20
+now=$(date)
+echo "Ny name is $name and I'm $myage years old"
+echo "The date of today is $now"
+echo "You are $USER"
+echo $now
+echo  "My name is $USER "
+
